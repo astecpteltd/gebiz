c48e17996a0c75f3cf44e6b7c398e576a3a36e5e
lamhotjm <lamhot.siagian@modalku.co.id>
2016-10-19 18:20:13 +0700

    combine with selector to convert respond to html

--- gebiz.py
+++ gebiz.py
@@ -1,10 +1,7 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from scrapy.selector import Selector
-
+import time
 
 class testGebizDriver(unittest.TestCase):
     def setUp(self):
@@ -21,30 +18,34 @@ class testGebizDriver(unittest.TestCase):
 
         self.driver.find_element_by_xpath(".//*[@id='contentForm:search']").click()
 
-        # while True:
-        # next = self.driver.find_element_by_xpath(".//*[@id='contentForm:j_idt104:j_idt116_" + str(i) + "_" + str(i) + "']")
-        self.driver.implicitly_wait(4)
-        for count in xrange(2, 10):
+        time.sleep(2)
+        for count in xrange(2, 832):
             self.driver.find_element_by_xpath(
                 ".//*[@id='contentForm:j_idt104:j_idt116_Next_" + str(count) + "']").click()
-            self.driver.implicitly_wait(4)
-            for i in range(0, 10):
-                self.driver.implicitly_wait(10)
-                companyName = self.driver.find_element_by_xpath(".//*[@class='commandLink_TITLE-BLUE']")
-                name = companyName.text if companyName.text else 'kosong'
-                companyPhone = self.driver.find_element_by_xpath(
-                    ".//*[@id]/div/table/tbody/tr/td/div/div/span/table/tbody/tr/td[2]")
-                phone = companyPhone.text if companyPhone.text else 'kosong'
+            time.sleep(2)
+            self.html = self.driver.page_source
+            self.scraping()
 
+        self.driver.quit()
+
+    def scraping(self):
+        for i in range(0, 10):
+            time.sleep(2)
+            try:
+                companyName = Selector(text=self.html).xpath(".//*[@class='commandLink_TITLE-BLUE']/text()").extract()
+                name = companyName[i].strip() if companyName else ''
+                companyPhone = Selector(text=self.html).xpath(
+                    ".//*/tbody/tr/td/div/div/span/table/tbody/tr/td[2]/text()").extract()
+                phone = companyPhone[i].strip() if companyPhone else ''
                 with open('sg-company.txt', 'a') as f:
                     f.write('{0};{1}\n'.format(name,
                                                phone))
+            except:
+                pass
+
 
     def tearDown(self):
         self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
